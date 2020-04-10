2e46def054f6394006baa1d4a166bbb0dde3c719
ivan <ivanbongiorni@gmail.com>
2020-04-10 23:06:26 +0200

    refactoring RNN_dataprep

--- tools.py
+++ tools.py
@@ -1,6 +1,10 @@
 """
+Author: Ivan Bongiorni,     https://github.com/IvanBongiorni
+2020-04-10
+
 PRE-PROCESSING TOOLS FOR INPUT PIPELINE.
 """
+
 import pickle
 import re
 import numpy as np
@@ -43,11 +47,10 @@ def process_url(url):
     return url_features
 
 
-def impute_nan(x):
+def mask_nan(x):
     """
-    Applies to forms of missing values imputation.
-    1. left NaN's mean the series didn't start and are imputed with zero
-    2. Other NaN's within the series are imputed with a ML model for imputations
+    Masks NaN values using zeros for left NaN's, placeholder values
+    from params for internal ones, cuts NaN's on the right.
     """
     import numpy as np
 
@@ -55,6 +58,8 @@ def impute_nan(x):
     if np.isnan(x[0]):
         cumsum = np.cumsum(np.isnan(x))
         x[ :np.argmax(cumsum[:-1]==cumsum[1:]) +1] = 0
+
+    # Measure fills internal NaN's with placeholder
 
     # Impute internal NaN's with imputation model
     if np.sum(np.isnan(x)) > 0:
@@ -64,33 +69,59 @@ def impute_nan(x):
         return x
 
 
-def scale_trends(df, path_to_data):
+# def scale_trends(df, path_to_data):
+#     """
+#     Scales trends divided by language groups. It splits df into sub-df's by
+#     language, and applies a robust custom scaling in two steps:
+#         1. log( x + 1 )
+#         2. Robust min-max scaling to 0 and 99th percentile (by language group)
+#     All 99th percentiles are saved in scaling_dict that is then pickled in
+#     .../data/ folder, to allow for repeated applications of the model.
+#     """
+#     import pickle
+#     import numpy as np
+#     import pandas as pd
+#
+#     scaling_dict = {}
+#
+#     for language in languages:
+#         sdf = df[ df['language'] == language ]
+#         page = sdf['Page']
+#         X = sdf.drop('Page', axis = 1).values
+#
+#         # log(x+1) and robust scale to [0, 99th percentile]
+#         X = np.log(X + 1)
+#         percentile_99th = np.percentile(X.reshape((X.size, )), 99)
+#         X = ( X - np.min(X) ) / ( percentile_99th - np.min(X) )
+#
+#         # store 99th percentile into scaling_dict
+#         scaling_dict[language] = percentile_99th
+#
+#     # pickle this dict into save_path
+#     d = open(path_to_params + 'scaling_dict.pkl','wb')
+#     pickle.dump(scaling_dict, f)
+#     d.close()
+#
+#     return df
+
+def scale_trends(df, params, language):
     """
-    Scales trends divided by language groups. It splits df into sub-df's by
-    language, and applies a robust custom scaling in two steps:
+    Takes a linguistic sub-dataframe and applies a robust custom scaling in two steps:
         1. log( x + 1 )
-        2. Robust min-max scaling to 0 and 99th percentile (by language group)
-    All 99th percentiles are saved in scaling_dict that is then pickled in
-    .../data/ folder, to allow for repeated applications of the model.
+        2. Robust min-max scaling to [ 0, 99th percentile ]
+    Percentiles are pickled as as ./data/scaling_dict.pkl for replications.
     """
     import pickle
     import numpy as np
     import pandas as pd
 
-    scaling_dict = {}
+    # log(x+1) and robust scale to [0, 99th percentile]
+    X = np.log(X + 1)
+    percentile_99th = np.percentile(X.reshape((X.size, )), 99)
+    X = ( X - np.min(X) ) / ( percentile_99th - np.min(X) )
 
-    for language in languages:
-        sdf = df[ df['language'] == language ]
-        page = sdf['Page']
-        X = sdf.drop('Page', axis = 1).values
-
-        # log(x+1) and robust scale to [0, 99th percentile]
-        X = np.log(X + 1)
-        percentile_99th = np.percentile(X.reshape((X.size, )), 99)
-        X = ( X - np.min(X) ) / ( percentile_99th - np.min(X) )
-
-        # store 99th percentile into scaling_dict
-        scaling_dict[language] = percentile_99th
+    # store 99th percentile into scaling_dict
+    scaling_dict[language] = percentile_99th
 
     # pickle this dict into save_path
     d = open(path_to_params + 'scaling_dict.pkl','wb')
@@ -100,7 +131,62 @@ def scale_trends(df, path_to_data):
     return df
 
 
-def RNN_dataprep(trend, len_input):
+# def RNN_dataprep(trend, len_input):
+#     """
+#     Main processing function.
+#     From each trend, returns 3D np.array defined by:
+#         ( no obs. , length input series , no input variables )
+#     Steps:
+#         1. From trend, trend vars and page vars, returns 2D np.array with one col
+#             per variable
+#         2. Creates empty 3D np.array, and fills it variable by variable by running
+#             _univariate_processing(). It takes a variable, turns it into a 2D
+#             matrix, then pastes into the final 3D matrix as a slice
+#     """
+#     import numpy as np
+#
+#     def _univariate_processing(variable, window):
+#         '''Process single vars, gets a 'sliding window' 2D array out of a 1D var'''
+#         import numpy as np
+#         V = np.empty((len(variable)-window+1, window))  # 2D matrix from variable
+#         for i in range(V.shape[0]):
+#             V[i,:] = variable[i : i+window]
+#         return V.astype(np.float32)
+#
+#     page_vars = trend[ -16: ]  # last 16 cols are page data
+#
+#     x = trend[ 365:-16 ]   # actual time series
+#     # trend = trend[ 365:-16 ]
+#
+#     # Missing values imputation
+#
+#
+#     trend_lag_year = trend[ 0:len(x) ]   # 365 days ahead
+#     trend_lag_quarter =  = trend[ 90:len(trend)+90 ]  # 90 days ahead
+#
+#     # scalar page vars (language, website, access, agent) are repeated for whole col
+#     page_vars = np.column_stack([ np.repeat(j, len(trend)) for j in page_vars ])
+#
+#     # Make a 2D matrix for each trend, composed of:
+#     T = np.column_stack([
+#         x,
+#         trend_lag_year,     # year time lag
+#         trend_lag_quarter,  # quarter time lag
+#         page_vars,          # page vars
+#         weekdays, yeardays  # temporal position in week and year (scaled to [0, 1])
+#     ])
+#
+#     # Use data from T to fill preprocessed matrix for RNN
+#     X = np.empty((T.shape[0]-len_input+1, len_input, T.shape[1]))
+#
+#     for i in range(T.shape[1]):
+#         X[ : , : , i ] = _univariate_processing(T[:,i], len_input)
+#
+#     X = X.astype(np.float32)
+#     return X
+
+
+def RNN_dataprep(x, page, params):
     """
     Main processing function.
     From each trend, returns 3D np.array defined by:
@@ -122,21 +208,6 @@ def RNN_dataprep(trend, len_input):
             V[i,:] = variable[i : i+window]
         return V.astype(np.float32)
 
-    page_vars = trend[ -16: ]  # last 16 cols are page data
-
-    x = trend[ 365:-16 ]   # actual time series
-    # trend = trend[ 365:-16 ]
-
-    # Missing values imputation
-
-
-    trend_lag_year = trend[ 0:len(x) ]   # 365 days ahead
-    trend_lag_quarter =  = trend[ 90:len(trend)+90 ]  # 90 days ahead
-
-    # scalar page vars (language, website, access, agent) are repeated for whole col
-    page_vars = np.column_stack([ np.repeat(j, len(trend)) for j in page_vars ])
-
-    # Make a 2D matrix for each trend, composed of:
     T = np.column_stack([
         x,
         trend_lag_year,     # year time lag
