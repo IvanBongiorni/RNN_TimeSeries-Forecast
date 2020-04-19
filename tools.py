343ee3bfd75ede48b70e0e4f1c4976a4f189fbde
ivan <ivanbongiorni@gmail.com>
2020-04-19 15:36:05 +0200

    prototyped rnn dataprep

--- tools.py
+++ tools.py
@@ -58,41 +58,6 @@ def left_fill_nan(x):
     return x
 
 
-# def scale_trends(df, path_to_data):
-#     """
-#     Scales trends divided by language groups. It splits df into sub-df's by
-#     language, and applies a robust custom scaling in two steps:
-#         1. log( x + 1 )
-#         2. Robust min-max scaling to 0 and 99th percentile (by language group)
-#     All 99th percentiles are saved in scaling_dict that is then pickled in
-#     .../data/ folder, to allow for repeated applications of the model.
-#     """
-#     import pickle
-#     import numpy as np
-#     import pandas as pd
-#
-#     scaling_dict = {}
-#
-#     for language in languages:
-#         sdf = df[ df['language'] == language ]
-#         page = sdf['Page']
-#         X = sdf.drop('Page', axis = 1).values
-#
-#         # log(x+1) and robust scale to [0, 99th percentile]
-#         X = np.log(X + 1)
-#         percentile_99th = np.percentile(X.reshape((X.size, )), 99)
-#         X = ( X - np.min(X) ) / ( percentile_99th - np.min(X) )
-#
-#         # store 99th percentile into scaling_dict
-#         scaling_dict[language] = percentile_99th
-#
-#     # pickle this dict into save_path
-#     d = open(path_to_params + 'scaling_dict.pkl','wb')
-#     pickle.dump(scaling_dict, f)
-#     d.close()
-#
-#     return df
-
 @numba.jit(python = True)
 def scale_trends(X, params, language):
     """
@@ -125,60 +90,6 @@ def scale_trends(X, params, language):
 
     return df
 
-
-# def RNN_dataprep(trend, len_input):
-#     """
-#     Main processing function.
-#     From each trend, returns 3D np.array defined by:
-#         ( no obs. , length input series , no input variables )
-#     Steps:
-#         1. From trend, trend vars and page vars, returns 2D np.array with one col
-#             per variable
-#         2. Creates empty 3D np.array, and fills it variable by variable by running
-#             _univariate_processing(). It takes a variable, turns it into a 2D
-#             matrix, then pastes into the final 3D matrix as a slice
-#     """
-#     import numpy as np
-#
-#     def _univariate_processing(variable, window):
-#         '''Process single vars, gets a 'sliding window' 2D array out of a 1D var'''
-#         import numpy as np
-#         V = np.empty((len(variable)-window+1, window))  # 2D matrix from variable
-#         for i in range(V.shape[0]):
-#             V[i,:] = variable[i : i+window]
-#         return V.astype(np.float32)
-#
-#     page_vars = trend[ -16: ]  # last 16 cols are page data
-#
-#     x = trend[ 365:-16 ]   # actual time series
-#     # trend = trend[ 365:-16 ]
-#
-#     # Missing values imputation
-#
-#
-#     trend_lag_year = trend[ 0:len(x) ]   # 365 days ahead
-#     trend_lag_quarter =  = trend[ 90:len(trend)+90 ]  # 90 days ahead
-#
-#     # scalar page vars (language, website, access, agent) are repeated for whole col
-#     page_vars = np.column_stack([ np.repeat(j, len(trend)) for j in page_vars ])
-#
-#     # Make a 2D matrix for each trend, composed of:
-#     T = np.column_stack([
-#         x,
-#         trend_lag_year,     # year time lag
-#         trend_lag_quarter,  # quarter time lag
-#         page_vars,          # page vars
-#         weekdays, yeardays  # temporal position in week and year (scaled to [0, 1])
-#     ])
-#
-#     # Use data from T to fill preprocessed matrix for RNN
-#     X = np.empty((T.shape[0]-len_input+1, len_input, T.shape[1]))
-#
-#     for i in range(T.shape[1]):
-#         X[ : , : , i ] = _univariate_processing(T[:,i], len_input)
-#
-#     X = X.astype(np.float32)
-#     return X
 
 @numba.jit(python = True)
 def right_trim_nan(x):
@@ -234,39 +145,38 @@ def RNN_dataprep(t, page_vars, day_week, day_year, params):
 
     if len(t) < 365 + params['len_input'] + params['len_prediction']:
         return None
-    else:
-        trend_lag_year = t[ :-365 ]
-        trend_lag_quarter = t[ 180:-180 ]
 
-        t = t[ 365: ]
+    # Cut trend and time lags
+    trend_lag_year = t[ :-365 ]
+    trend_lag_quarter = t[ 180:-180 ]
+    t = t[ 365: ]
 
-        day_week = day_week[ :len(t) ]
-        day_year = day_year[ :len(t) ]
+    # Add weekday and year day information
+    day_week = day_week[ :len(t) ]
+    day_year = day_year[ :len(t) ]
 
-        # Multiply page data on the whole
-        # language = np.repeat(page_vars[0], len(t))
-        # website = np.repeat(page_vars[1], len(t))
-        # access = np.repeat(page_vars[2], len(t))
-        # agent = np.repeat(page_vars[3], len(t))
+    # Make a 2D matrix of trend data
+    T = np.column_stack([
+        t,
+        trend_lag_quarter,
+        trend_lag_year,
+        weekdays,
+        yeardays
+    ])
 
-        T = np.column_stack([
-            t
-            trend_lag_quarter,
-            trend_lag_year,
+    # Attach page variables in the same format
+    page_vars = np.repeat(p, len(t)).reshape((len(page_vars), len(t))).T #make 2D
+    T = np.hstack([ T, page_vars ])
 
-            ### TODO: IMPORTANTE _ DEVO ESTENDERE I DATI DI page_vars 0
-            ### Arrivano come un vettore 1D
+    # Apply actual RNN preprocessing
 
+    # X_processed = np.empty((T.shape[0]-params['len_input']+1, params['len_input'], T.shape[1]))
 
-            page_vars,
+    # X_processed = []
+    # for i in range(T.shape[1]):
+    #     X_processed.append( univariate_processing(T[:,i], len_input) )
 
-            weekdays,
-            yeardays
-        ])
-
-        T = np.empty((T.shape[0]-params['len_input']+1, params['len_input'], T.shape[1]))
-
-        for i in range(T.shape[1]):
-            T[ : , : , i ] = univariate_processing(T[:,i], len_input)
-
-        return T
+    X_processed = [ univariate_processing(T[:,i], len_input) for i in range(T.shape[1]) ]
+    X_processed = np.concatenate(X_processed)
+    
+    return X_processed
