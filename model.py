4837e284d98df8327635ab8184699557f67a51c2
IvanBongiorni <ivanbongiorni@gmail.com>
2020-08-28 18:14:29 +0200

    train to separate module + refactoring in prog

--- model.py
+++ model.py
@@ -55,52 +55,46 @@ def build(params):
     return model
 
 
-# def build_GAN():
-#     return G, A
+#def train(model, params, X_train, Y_train, X_val, Y_val):
+#    import time
+#    import numpy as np
+#    import tensorflow as tf
+#
+#    optimizer = tf.keras.optimizers.Adam(learning_rate = params['learning_rate'])
+#
+#    @tf.function
+#    def train_on_batch():
+#        take = iteration * batch_size
+#        X_batch = X_train[ take:take+batch_size , : ]
+#        Y_batch = Y_train[ take:take+batch_size , : ]
+#
+#        with tf.GrandientTape() as tape:
+#            current_loss = tf.keras.losses.BinaryCrossentropy(Y_batch, model(X_batch))
+#        gradients = tape.gradient(current_loss, model.trainable_variables)
+#        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+#        return current_loss
+#
+#    for epoch in range(params['n_epochs']):
+#        start = time.time()
+#
+#        for iteration in range(X_train.shape[0] // params['batch_size']):
+#            current_loss = train_on_batch()
+#            # loss_history.append(current_loss)
+#
+#        validation_loss = tf.reduce_mean(
+#            tf.keras.losses.sparse_categorical_crossentropy(Y_val, model(X_val),
+#                                                            from_logits = True))
+#
+#        print('{}.   \tTraining Loss: {}   \tValidation Loss: {}   \tTime: {}ss'.format(
+#            epoch,
+#            current_loss.numpy(),
+#            validation_loss.numpy(),
+#            round(time.time()-start, 2)
+#        ))
+#    print('Training complete.\n')
+#
+#    model.save('{}/{}.h5'.format(params['save_path'], params['model_name']))
+#    print('Model saved at:\n\t{}'.format(params['save_path']))
+#    return None
 
 
-def train(model, params, X_train, Y_train, X_val, Y_val):
-    import time
-    import numpy as np
-    import tensorflow as tf
-
-    optimizer = tf.keras.optimizers.Adam(learning_rate = params['learning_rate'])
-
-    @tf.function
-    def train_on_batch():
-        take = iteration * batch_size
-        X_batch = X_train[ take:take+batch_size , : ]
-        Y_batch = Y_train[ take:take+batch_size , : ]
-
-        with tf.GrandientTape() as tape:
-            current_loss = tf.keras.losses.BinaryCrossentropy(Y_batch, model(X_batch))
-        gradients = tape.gradient(current_loss, model.trainable_variables)
-        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-        return current_loss
-
-    for epoch in range(params['n_epochs']):
-        start = time.time()
-
-        for iteration in range(X_train.shape[0] // params['batch_size']):
-            current_loss = train_on_batch()
-            # loss_history.append(current_loss)
-
-        validation_loss = tf.reduce_mean(
-            tf.keras.losses.sparse_categorical_crossentropy(Y_val, model(X_val),
-                                                            from_logits = True))
-
-        print('{}.   \tTraining Loss: {}   \tValidation Loss: {}   \tTime: {}ss'.format(
-            epoch,
-            current_loss.numpy(),
-            validation_loss.numpy(),
-            round(time.time()-start, 2)
-        ))
-    print('Training complete.\n')
-
-    model.save('{}/{}.h5'.format(params['save_path'], params['model_name']))
-    print('Model saved at:\n\t{}'.format(params['save_path']))
-    return None
-
-
-# def train_GAN():
-#     return None
