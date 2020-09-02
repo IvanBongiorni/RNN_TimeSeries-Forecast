e30935ebaa06758b965e3ccaf4116c513c64226d
IvanBongiorni <ivanbongiorni@gmail.com>
2020-09-02 22:48:53 +0200

    fixed weights initialization, cleaning

--- model.py
+++ model.py
@@ -5,17 +5,6 @@ Author: Ivan Bongiorni,     https://github.com/IvanBongiorni
 Contains model implementations.
 """
 
-def _build_singlestep_RNN(params):
-    import tensorflow as tf
-    from tensorflow.keras.models import Sequential
-    from tensorflow.keras.layers import LSTM, TimeDistributed, Dense
-
-    RNN = Sequential([
-        LSTM(params['lstm_size'], input_shape=(None, 17)),
-        Dense(params['len_prediction'], activation='relu', kernel_initializer=tf.keras.initializers.Zeros())
-    ])
-    return rnn_regressor
-
 
 def _build_multistep_RNN(params):
     import tensorflow as tf
@@ -24,7 +13,7 @@ def _build_multistep_RNN(params):
 
     RNN = Sequential([
         LSTM(params['lstm_size'], input_shape=(None, 17)),
-        Dense(params['len_prediction'], activation='relu', kernel_initializer=tf.keras.initializers.Zeros())
+        Dense(params['len_prediction'], activation='relu')
     ])
     return RNN
 
@@ -37,7 +26,7 @@ def _build_seq2seq_regressor(params):
     RNN = Sequential([
         LSTM(params['len_input'], input_shape=(None, 17), return_sequences=True),
         LSTM(params['len_input'], return_sequences=True),
-        TimeDistributed(Dense(params['dense_size'], activation='relu', kernel_initializer=tf.keras.initializers.Zeros()))
+        TimeDistributed(Dense(params['dense_size'], activation='relu'))
     ])
     return RNN
 
